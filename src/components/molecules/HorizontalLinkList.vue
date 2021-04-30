6293fcc326814614645009b4c11febffe38be4fb
KIM-DONGJU <58763448+KIM-DONGJU@users.noreply.github.com>
2021-04-30 11:28:30 +0900

    Molecules에 새로운 Molecule 추가
    1. HorizontalLinkList.vue
    	- AppLink를 가로로 나열한 Molecule
    	- Navbar에 쓰일 에정

---
+++ src/components/molecules/HorizontalLinkList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="horizontal-link-list">
+    <ul>
+      <li v-for="link in links" :key="link.label">
+        <AppLink
+          :label="link.label"
+          :path="link.path"
+          :linkClass="link.linkClass"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import AppLink from "../atoms/AppLink";
+
+export default {
+  name: "HorizontalLinkList",
+  components: {
+    AppLink,
+  },
+  data() {
+    return {};
+  },
+  props: {
+    links: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.horizontal-link-list {
+  width: 100%;
+}
+
+.horizontal-link-list ul {
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+}
+.horizontal-link-list ul li {
+  flex: 1;
+}
+</style>
