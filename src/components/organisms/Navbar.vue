e683f739b4cf7726ab5c106a902935b338f674a6
KIM-DONGJU <58763448+KIM-DONGJU@users.noreply.github.com>
2021-05-01 18:10:59 +0900

    organisms/Navbar.vue 추가
    1. organisms에 Navbar 기능의 Navbar.vue 생성
    	- AppLogo와 HorizontalLinkList를 사용하여 생성
    
    2. organisms 요소들을 볼 수 있는 pages/organisms.vue 생성
    	- 이를 위해 router/index.js에 path 추가

---
+++ src/components/organisms/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="nav-bar">
+    <div class="logo">
+      <AppLogo logoClass="middle" />
+    </div>
+    <div class="nav">
+      <HorizontalLinkList :links="links" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppLogo from "../atoms/AppLogo";
+import HorizontalLinkList from "../molecules/HorizontalLinkList";
+
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      links: [
+        {
+          label: "atoms",
+          path: "/atoms",
+          linkClass: "font-color-black",
+        },
+        {
+          label: "molecules",
+          path: "/molecules",
+          linkClass: "font-color-black",
+        },
+        {
+          label: "organisms",
+          path: "/organisms",
+          linkClass: "font-color-black",
+        },
+        {
+          label: "templates",
+          path: "/templates",
+          linkClass: "font-color-black",
+        },
+        {
+          label: "pages",
+          path: "/pages",
+          linkClass: "font-color-black",
+        },
+      ],
+    };
+  },
+  components: {
+    AppLogo,
+    HorizontalLinkList,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-bar {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.logo {
+  flex: 1;
+}
+
+.nav {
+  flex: 9;
+}
+</style>
---
+++ src/components/pages/organisms.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="organisms-list">
+    <div class="navbar-container">
+      <h2 class="title">Navbar Example</h2>
+      <Navbar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../organisms/Navbar.vue";
+
+export default {
+  name: "Atoms",
+  components: {
+    Navbar,
+  },
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
