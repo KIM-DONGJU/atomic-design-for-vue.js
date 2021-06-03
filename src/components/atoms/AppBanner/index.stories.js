a3dc69652d6298253528d1b811403e70ec75c5a4
KIM-DONGJU <kdj0689@gmail.com>
2021-06-03 15:37:01 +0900

    atoms/AppBanner 추가

---
+++ src/components/atoms/AppBanner/index.stories.js
@@ -0,0 +1,60 @@
+import AppButton from './index.vue';
+
+export default {
+  title: 'Atoms/AppButton',
+  component: AppButton,
+  argTypes: {
+    buttonClass: {
+      control: {
+        type:"check",
+        options: ['shape-square', 'bg-white', 'bg-gray', 'left', 'right']
+      }
+    }
+  },
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { AppButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<AppButton v-bind="args" style="width: 20%;/>'
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Button',
+};
+
+export const ShapeSquare = Template.bind({});
+ShapeSquare.args = {
+  label: 'shape square',
+  buttonClass: 'shape-square'
+};
+
+export const BackgroundWhite = Template.bind({});
+BackgroundWhite.args = {
+  label: 'background white',
+  buttonClass: 'bg-white'
+};
+
+export const BackgroundGray = Template.bind({});
+BackgroundGray.args = {
+  label: 'background gray',
+  buttonClass: 'bg-gray'
+};
+
+export const TextLeft = Template.bind({});
+TextLeft.args = {
+  label: 'text left',
+  buttonClass: 'left'
+};
+
+export const BackgroundWhiteAndTextRight = Template.bind({});
+BackgroundWhiteAndTextRight.args = {
+  label: 'text left',
+  buttonClass: 'bg-white right'
+};
---
+++ src/components/atoms/AppBanner/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="image">
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  data() {
+    return {};
+  },
+  props: {
+    image: String,
+  },
+};
+</script>
+
+<style scoped>
+/* default style */
+.banner img {
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  display: block;
+  height: 100%;
+}
+
+/* font position style end*/
+</style>
